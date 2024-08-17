03fcb262da8e7828ebf09b97335c55725c6c51a7
Aditya Kumar Gupta <adityakumargupta2101@gmail.com>
2024-08-18 02:14:30 +0530

    first commit

--- server.js
+++ server.js
@@ -9,18 +9,18 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 
 // Proxy route to forward requests to the Swiggy API
-app.get('/api/*', async (req, res) => {
-  const apiUrl = `https://www.swiggy.com${req.originalUrl.replace('/api', '')}`;
+app.get('/dapi/*', async (req, res) => {
+  const apiUrl = `https://www.swiggy.com${req.originalUrl}`;
 
   try {
     const response = await axios.get(apiUrl);
     res.json(response.data);
   } catch (error) {
-    res.status(error.response.status).json({ error: error.message });
+    res.status(error.response ? error.response.status : 500).json({ error: error.message });
   }
 });
 
-// Serve the React app (if you want to serve static files)
+// Serve static files from the React app (optional)
 app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('*', (req, res) => {
