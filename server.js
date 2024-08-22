f8c449d8bd07592e2835f401cc6392d26aaeefe4
Aditya Kumar Gupta <adityakumargupta2101@gmail.com>
2024-08-22 16:52:53 +0530

    new change

--- server.js
+++ server.js
@@ -1,37 +1,17 @@
 const express = require('express');
 const cors = require('cors');
 const fetch = require('cross-fetch');
-const NodeCache = require('node-cache');
-const compression = require('compression');
 
 const app = express();
 const port = process.env.PORT || 5000;
-const cache = new NodeCache({ stdTTL: 300 }); // Cache for 5 minutes
 
 app.use(cors());
-app.use(compression()); // Enable GZIP compression
-
-// Middleware for caching
-const cacheMiddleware = (req, res, next) => {
-    const key = req.originalUrl;
-    const cachedResponse = cache.get(key);
-
-    if (cachedResponse) {
-        console.log('Serving from cache:', key);
-        res.json(cachedResponse);
-    } else {
-        res.sendResponse = res.json;
-        res.json = (body) => {
-            cache.set(key, body);
-            res.sendResponse(body);
-        };
-        next();
-    }
-};
 
 // For Restaurant API
-app.get('/dapi/restaurants/list/v5', cacheMiddleware, async (req, res) => {
+app.get('/dapi/restaurants/list/v5', async (req, res) => {
     const { lat, lng, page_type } = req.query;
+    console.log(req.query);
+
     const url = `https://www.swiggy.com/dapi/restaurants/list/v5?lat=${lat}&lng=${lng}&page_type=${page_type}`;
 
     try {
@@ -47,17 +27,19 @@ app.get('/dapi/restaurants/list/v5', cacheMiddleware, async (req, res) => {
         }
 
         const data = await response.json();
-        console.log('Data fetched:', data);
+        console.log(data);
         res.json(data);
     } catch (error) {
-        console.error('Error fetching restaurant data:', error);
+        console.error(error);
         res.status(500).send('An error occurred');
     }
 });
 
 // For Menu API
-app.get('/dapi/menu/pl', cacheMiddleware, async (req, res) => {
+app.get('/dapi/menu/pl', async (req, res) => {
     const { 'page-type': page_type, 'complete-menu': complete_menu, lat, lng, submitAction, restaurantId } = req.query;
+    console.log(req.query);
+
     const url = `https://www.swiggy.com/dapi/menu/pl?page-type=${page_type}&complete-menu=${complete_menu}&lat=${lat}&lng=${lng}&submitAction=${submitAction}&restaurantId=${restaurantId}`;
 
     try {
@@ -73,15 +55,14 @@ app.get('/dapi/menu/pl', cacheMiddleware, async (req, res) => {
         }
 
         const data = await response.json();
-        console.log('Data fetched:', data);
+        console.log(data);
         res.json(data);
     } catch (error) {
-        console.error('Error fetching menu data:', error);
+        console.error(error);
         res.status(500).send('An error occurred');
     }
 });
 
-// Root Route
 app.get('/', (req, res) => {
     res.json({ "test": "Welcome to ApnaKitchen! - See Live Web URL for this Server - https://apnakitchenlive.onrender.com" });
 });
